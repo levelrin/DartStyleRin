c4e5176b231dddc50699d9c7259ab0d1f8b1641a
Rin <levelrin@gmail.com>
2020-04-21 03:09:00 +0900

    Check the format of the source code during CI. fixes #1

--- ci.sh
+++ ci.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+set -o pipefail
+
 echo "Build Start"
 
-pub get || { exit 1; }
+pub get
 
-analyzerReport=$(dartanalyzer lib test) || { exit 1; }
+analyzerReport=$(dartanalyzer lib test)
 expectedAnalyzerReport="Analyzing lib, test...
 No issues found!"
 echo "$analyzerReport"
@@ -12,10 +15,12 @@ if [ "$analyzerReport" != "$expectedAnalyzerReport" ]; then
   exit 1
 fi
 
-pub run test || { exit 1; }
+dart bin/stylerin.dart --check
+
+pub run test
 
 threshold=90
-coverageReport=$(pub run test_coverage --no-badge) || { exit 1; }
+coverageReport=$(pub run test_coverage --no-badge)
 coverageRegex="Overall line coverage rate: ([0-9]*[.]?[0-9]+)%[.]"
 echo "$coverageReport"
 echo "Coverage Threshold: $threshold%"
