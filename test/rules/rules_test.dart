2c56422673a6745e21d3ee1924ce42a8f6e2e370
Rin <levelrin@gmail.com>
2020-05-05 18:17:27 +0900

    Test the Rules.format() if it applies all rules correctly. refs #8

--- test/rules/rules_test.dart
+++ test/rules/rules_test.dart
@@ -7,12 +7,14 @@
 
 import 'package:dart_style_rin/feedback/feedback.dart';
 import 'package:dart_style_rin/line/line.dart';
+import 'package:dart_style_rin/log/log.dart';
 import 'package:dart_style_rin/log/silent_log.dart';
 import 'package:dart_style_rin/rule/confirmable_rule.dart';
 import 'package:dart_style_rin/rule/leaked_check_rule.dart';
 import 'package:dart_style_rin/rule/rule.dart';
 import 'package:dart_style_rin/rules/rules.dart';
 import 'package:dart_style_rin/source/ignored_source.dart';
+import 'package:dart_style_rin/source/source.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -40,7 +42,7 @@ void main() {
         false
       );
     });
-    test('.format() should return 2 if there was any erros on formatting.', () {
+    test('.format() should apply Rule.format().', () {
       final ConfirmableRule rule = ConfirmableRule();
       Rules(
         <Rule>[rule],
@@ -50,5 +52,45 @@ void main() {
       );
       expect(rule.formatCalled, true);
     });
+    test('.format() should return the source, in which all rules are applied.', () {
+      final SilentLog log = SilentLog();
+      expect(
+        Rules(
+          <Rule>[
+            _AppendRule('Banana', log),
+            _AppendRule('Orange', log)
+          ],
+          log
+        ).format(
+          Source('Apple', log)
+        ),
+        Source('AppleBananaOrange', log)
+      );
+    });
   });
+}
+
+/// It's for testing.
+/// It appends a text to the source code.
+class _AppendRule implements Rule {
+
+  /// Constructor.
+  const _AppendRule(this._text, this._log);
+
+  /// We will append this to the source code.
+  final String _text;
+
+  /// For logging.
+  final Log _log;
+
+  @override
+  List<Feedback> check(final Source source) {
+    throw UnimplementedError('Please define the behavior.');
+  }
+
+  @override
+  Source format(final Source source) {
+    return Source('${source.toString()}$_text', _log);
+  }
+
 }
