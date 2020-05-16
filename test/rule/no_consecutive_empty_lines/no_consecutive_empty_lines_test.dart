5e5d08bff761dc197f9ae5574e7841739a945d9a
Rin <levelrin@gmail.com>
2020-05-17 03:26:17 +0900

    Extract the test data from the no_consecutive_empty_lines_test.dart. refs #3

--- test/rule/no_consecutive_empty_lines/no_consecutive_empty_lines_test.dart
+++ test/rule/no_consecutive_empty_lines/no_consecutive_empty_lines_test.dart
@@ -2,6 +2,7 @@
 /// This file has been created under the terms of the MIT License.
 /// See the details at https://github.com/levelrin/DartStyleRin/blob/master/LICENSE
 
+import 'dart:io' as io;
 import 'package:dart_style_rin/feedback/feedback.dart';
 import 'package:dart_style_rin/log/silent_log.dart';
 import 'package:dart_style_rin/rule/no_consecutive_empty_lines.dart';
@@ -9,11 +10,23 @@ import 'package:dart_style_rin/source/source.dart';
 import 'package:test/test.dart';
 
 void main() {
+
+  /// Invalid content.
+  String invalid;
+
+  /// Valid content.
+  String valid;
+
+  setUpAll(() {
+    invalid = io.File('test/rule/no_consecutive_empty_lines/invalid.txt').readAsStringSync();
+    valid = io.File('test/rule/no_consecutive_empty_lines/valid.txt').readAsStringSync();
+  });
+
   group('NoConsecutiveEmptyLines', () {
     test('.check() should give feedback if consecutive empty lines were found.', () {
       final SilentLog log = SilentLog();
       final List<Feedback> feedbackList = NoConsecutiveEmptyLines(log).check(
-        Source(_invalid(), log)
+        Source(invalid, log)
       );
       expect(
         feedbackList.length,
@@ -23,7 +36,7 @@ void main() {
     test('.check() should not give feedback if consecutive empty lines were not found.', () {
       final SilentLog log = SilentLog();
       final List<Feedback> feedbackList = NoConsecutiveEmptyLines(log).check(
-        Source(_valid, log)
+        Source(valid, log)
       );
       expect(feedbackList, isEmpty);
     });
@@ -31,37 +44,10 @@ void main() {
       final SilentLog log = SilentLog();
       expect(
         NoConsecutiveEmptyLines(log).format(
-          Source(_invalid(), log)
+          Source(invalid, log)
         ),
-        Source(_valid, log)
+        Source(valid, log)
       );
     });
   });
 }
-
-/// Remove the placeholder and return the invalid code.
-String _invalid() {
-  return _invalidWithPlaceholder.replaceAll('*', '\n');
-}
-
-const String _invalidWithPlaceholder = '''
-*
-*
-class Apple {
-*
-  String color() {
-    return "Red";
-  }
-
-}*
-''';
-
-const String _valid = '''
-class Apple {
-
-  String color() {
-    return "Red";
-  }
-
-}
-''';
